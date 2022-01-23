a0a4a244a3ffcc43112ea6eb41d00363199ae375
Meziane <Meziane@LAPTOP-E6F4EQ7Q>
2022-01-23 13:38:41 +0100

    Ajoutez des fichiers projet.

---
+++ SchoolApi/Migrations/20210731143648_InitialCreate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SchoolApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Courses");
+        }
+    }
+}
---
+++ SchoolApi/Models/SchoolApiContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolApi.Models
+{
+    public class SchoolApiContext : DbContext
+    {
+        public SchoolApiContext(DbContextOptions<SchoolApiContext> options)
+            :base(options)
+        {
+
+        }
+        public DbSet<Course> Courses { get; set; }
+    }
+}
